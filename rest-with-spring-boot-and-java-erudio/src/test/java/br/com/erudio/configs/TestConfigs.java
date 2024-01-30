2ddeb1cd64bf278c661a204f7df6ed9bb26b8bf6
realkodesh <liraguilherme186@gmail.com>
2024-01-30 07:34:24 -0300

    Projeto até Dockerização 2

--- rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/configs/TestConfigs.java
+++ rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/configs/TestConfigs.java
@@ -1,8 +1,8 @@
 package br.com.erudio.configs;
 
 public class TestConfigs {
-
-	public static final int SERVER_PORT = 80;
+  
+	public static final int SERVER_PORT = 8080;
 	
 	public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
 	public static final String HEADER_PARAM_ORIGIN = "Origin";
